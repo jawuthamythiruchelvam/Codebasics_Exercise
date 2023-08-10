ba2b82f0757bd4a77fedd6eaa55cf5a5436bb403
jawuthamy <thirujawuthamy007@gmail.com>
2023-08-10 13:26:53 +0530

    variables exercise

---
+++ variables.py
@@ -0,0 +1,17 @@
+#q1
+# we can not add  values for keywords
+#break=5 it will make error
+#q2
+birth_year=2001
+current_year=2023
+my_age=current_year-birth_year
+print(my_age)
+
+#q3
+first_name="jawuthamy"
+last_name="thiruchelvam"
+print(f"my full name is {first_name}  {last_name}")
+
+#q4
+variables=["_nation"," 1record"," record1", "record_one", "record^one", "continue"]
+# record^one , 1record, continue these are invalid variable names
