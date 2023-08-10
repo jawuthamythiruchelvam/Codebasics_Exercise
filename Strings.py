cfebab33f0b85d7746b712185224de4ec7513d82
jawuthamy <thirujawuthamy007@gmail.com>
2023-08-10 14:32:42 +0530

    number and string exercise

---
+++ Strings.py
@@ -0,0 +1,21 @@
+#1
+street = "visvahulam lane"
+city = "point_pedro"
+country = "Srilanka"
+address1 =street+'\n'+city+'\n'+country
+address2 = f"{street} \n {city}\n {country} "
+print(address1)
+print(address2)
+#2
+STR="Earth revolves around the sun"
+print(STR[6:14])
+print(STR[-3:])
+#3
+x=5
+y= 3
+print(f"I eat {x} veggies and {y} fruits daily")
+#4
+line="maine 200 banana khaye"
+print(line)
+newline = line.replace('200','10').replace("banana", "samosa")
+print("new line is:", newline)
---
+++ numbers.py
@@ -0,0 +1,17 @@
+#1
+long = 92
+wide = 48.8
+area=long*wide
+print("foodball field area: ", round(area,2))
+#2
+nuOfpackets=9
+one_packet = 1.49
+money=20
+change=money-(nuOfpackets*one_packet)
+print("cash bake:", change)
+#3
+areaOfBathroom=5.5**2
+print("total cost:", areaOfBathroom)
+#4
+number=17
+print(f"binary value of {number} is:", bin(17)[2:])
