95092513115f160fdcbd1943e38d53ba5a6cc466
jawuthamy <thirujawuthamy007@gmail.com>
2023-08-15 11:41:40 +0530

    iterators exercise

---
+++ Iterators.py
@@ -0,0 +1,26 @@
+class Fibonacci:
+    def __init__(self,limit):
+        self.previous=0
+        self.current=1
+        self.limit=limit
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        result=self.current
+        if result<self.limit:
+            result=self.current+self.previous
+            self.previous=self.current
+            self.current=result
+            return result
+        else:
+            raise StopIteration
+
+fib_iterator = iter(Fibonacci(100))
+while True:
+    try:
+        print(next(fib_iterator))
+    except StopIteration:
+            break
